cd8d12dec87995f7a1b69b1b3c47fdc32c21b421
alishervoitov <voitovalisher020@gmail.com>
2024-06-11 15:41:12 +0500

    registration page

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from patients.models import Patient
+
+
+def register(request):
+    print(request.POST)
+    user_message: str = ''
+    password_message: str = ''
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name', None)
+        last_name = request.POST.get('last_name', None)
+        phone_number = request.POST.get('phone_number', None)
+        gender = request.POST.get('gender', None)
+        password = request.POST.get('password', None)
+        password1 = request.POST.get('password1', None)
+        user: Patient = Patient.objects.filter(phone_number=phone_number)
+        if user:
+            user = Patient.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                phone_number=phone_number,
+                gender=gender,
+                password=password
+            )
+            user.set_password(password)
+            user.save()
+            return redirect('home')
