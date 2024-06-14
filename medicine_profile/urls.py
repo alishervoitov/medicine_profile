aef8ee66f63be40af721c9b9d5ef92ac497da401
alishervoitov <voitovalisher020@gmail.com>
2024-06-14 15:53:51 +0500

    Urls

--- medicine_profile/urls.py
+++ medicine_profile/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from patients.views import log_in, register
+from django.conf.urls.static import static, settings
 
 urlpatterns = [
+    path('product/', include('product.urls')),
+    path('order/', include('order.urls')),
+    path('login/', log_in, name='login'),
+    path('register/', register, name='register'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(
+    settings.STATIC_URL,
+    document_root=settings.STATICFILES_DIRS
+)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
