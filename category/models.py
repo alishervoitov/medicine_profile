7c420a3e7d9050008231da6cc2da5ca44379a5d6
alishervoitov <voitovalisher020@gmail.com>
2024-06-22 14:24:13 +0500

    category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    def __str__(self):
+        return self.name
