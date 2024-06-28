d69872abff2571295708169ef308965557afd333
alishervoitov <voitovalisher020@gmail.com>
2024-06-28 19:08:06 +0500

    change the model of patients

--- patients/models.py
+++ patients/models.py
@@ -8,6 +8,21 @@ class Patient(AbstractUser):
         ('MALE', 'male'),
         ('FEMALE', 'female'),
     )
+    Region = (
+        ('Andijan', 'andijan'),
+        ('Namangan', 'namangan'),
+        ('Fergana', 'fergana'),
+        ('Tashkent city', 'tashkent city'),
+        ('Tashkent', 'tashkent'),
+        ('Syrdayra', 'syrdarya'),
+        ('Jizzakh', 'jizzakh'),
+        ('Samarkand', 'samarkand'),
+        ('Bukhara', 'bukhara'),
+        ('Kharazem', 'kharazem'),
+        ('Kashkadarya', 'kashkadarya'),
+        ('Surhandarya', 'syrkhandarya'),
+        ('Karakalpakstan', 'karakalpakstan'),
+    )
     phone_number = models.CharField(
         max_length=50,
         unique=True
@@ -19,15 +34,19 @@ class Patient(AbstractUser):
     )
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
-    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    birthday = models.CharField(max_length=20, null=True, blank=True)
     gender = models.CharField(max_length=10, null=True, choices=GENDER_CHOICE)
+    region = models.CharField(max_length=25, null=True, blank=True)
     password = models.CharField(max_length=255)
     email = models.EmailField(max_length=100, unique=True, null=True, blank=True)
     is_verified = models.BooleanField(default=False)
     image = models.ImageField(null=True, blank=True)
+    weight = models.IntegerField(blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
 
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = ["username", "email"]
+
     def __str__(self):
         if self.first_name and self.last_name:
             return f'{self.first_name} {self.last_name}'
@@ -37,6 +56,7 @@ class Patient(AbstractUser):
             return self.last_name
         else:
             return self.phone_number
+
 
 class Verification(models.Model):
     code = models.IntegerField(unique=True)
